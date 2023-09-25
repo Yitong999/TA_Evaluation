b9f510b5b2eb7f2603f8a08cc20af70fa61040ee
Yitong Tang <yitongta@dhcp-206-12-68-55.ubcsecure.wireless.ubc.ca>
2023-09-25 10:28:01 -0700

    update files clean features, and remove unnecessary files

--- main.py
+++ main.py
@@ -187,8 +187,20 @@ def write_to_docx(TA_name, data, comments):
 
     print('Table document created successfully.')
 
+def clean_docx_in_folder(folder_path):
+    # List all files in the folder
+    files = os.listdir(folder_path)
+
+    # Iterate through the files and delete .docx files
+    for file in files:
+        if file.endswith(".docx"):
+            file_path = os.path.join(folder_path, file)
+            os.remove(file_path)
+            print(f"Deleted: {file_path}")
 
 if __name__ == '__main__':
+    clean_docx_in_folder('Output')
+
     surveys = read_surveys()
     eval_results = eval_surveys(surveys)
     TA_sets = eval_results.TAs_set
